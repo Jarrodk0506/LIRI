a03cc7a5d0cf4f03c00ba19ecf9a7b8fede49259
Jarrod Krasnoff <Jarrodk0506@outlook.com>
2019-08-13 20:56:00 -0400

    Spotify works. Need to work on do-what-it-says and bands in town

--- liri.js
+++ liri.js
@@ -8,8 +8,7 @@ var spotify = new Spotify({
     id : keys.spotify.id,
     secret : keys.spotify.secret
 })
-   
-console.log("Please use 'commands' to see the list of commands");
+  
 
 
 
@@ -29,42 +28,51 @@ console.log("Please use 'commands' to see the list of commands");
 
 var command = process.argv[2];
 
-if(command === "commands"){ //display commands for the user
-    console.log("concert-this <insert band-name here> \nspotify-this-song <insert song-name here>"+ 
-                "\nmovie-this <insert movie-name here> \ndo-what-it-says");
-}
+
 
 if(command === "do-what-it-says"){
     fs.readFile("random.txt", "utf8", function(error, data){
         console.log("do works"); //delete this check\\
         console.log(data);
-        if(error){
-            return console.log(error);
-        }
+       
         
-        
-
     });
 }
 
-
+                                            //begin spotify
 if(command === "spotify-this-song"){
-console.log("song worked")//delete this check\\
-
-var song = process.argv[3]; // get song name from user
+// get song name from user
+var song = "";
+    for(i = 3; i < process.argv.length; i++){
+       song += process.argv[i] + " ";
+    }
 
 spotify.search({ type: 'track', query: song, limit: 1 }, function(err, data) {
     if (err) {
       return console.log('Error occurred: ' + err);
     }
-                            //look inside items
-    console.log(data.tracks.items); 
+
+    //catching paths into variables
+    var songName = (data.tracks.items[0].name);
+    var artist = (data.tracks.items[0].album.artists[0].name);
+    var album = data.tracks.items[0].album.name;
+    var link = data.tracks.items[0].album.external_urls.spotify;
+
+
+    console.log("Song Name: " + songName + 
+                "\nArtist Name: " + artist +
+                "\nAlbum Name: " + album +
+                "\nClick here to listen! " + link);
     
 
-  });
+ 
+
+  }); 
 }
 
-//begin Bands In Town
+
+
+                                            //begin Bands In Town\\
 if(command === "concert-this"){
 console.log("band worked"); //delete this check\\
 
@@ -72,14 +80,16 @@ var artist = process.argv[3]; // get band name from user
 
 var bandUrl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"; // use artist to make search URL
 axios.get(bandUrl).then(function(response){
-console.log(response);
+    for (i in response) {
+        console.log(response.request.ClientRequest);
+      }
 
 });
 }
 
 
 
-// begin OMBD
+                                            // begin OMBD
 if(command === "movie-this"){
    console.log("movie worked");//delete this check\\
 
@@ -104,4 +114,36 @@ axios.get(movieUrl).then(function(response){ //take url and get movie stats
 
     //console.log(respone.data.Ratings.Source) <-- beginning to get to Rotten tomatoes 
 });
+}
+
+
+
+function spotifySearch(){
+    // get song name from user
+    var song = "";
+        for(i = 3; i < process.argv.length; i++){
+           song += process.argv[i] + " ";
+        }
+    
+    spotify.search({ type: 'track', query: song, limit: 1 }, function(err, data) {
+        if (err) {
+          return console.log('Error occurred: ' + err);
+        }
+    
+        //catching paths into variables
+        var songName = (data.tracks.items[0].name);
+        var artist = (data.tracks.items[0].album.artists[0].name);
+        var album = data.tracks.items[0].album.name;
+        var link = data.tracks.items[0].album.external_urls.spotify;
+    
+    
+        console.log("Song Name: " + songName + 
+                    "\nArtist Name: " + artist +
+                    "\nAlbum Name: " + album +
+                    "\nClick here to listen! " + link);
+        
+    
+     
+    
+    });
 }
